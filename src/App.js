c165850bb17dfe33b509efa30c12ecb6f9215dc2
Alvaro Pimentel <alvaroaugusto@alvaropimentel.eti.br>
2023-07-23 20:43:15 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import kanye from "./image/kanye.jpg"
 import './App.css';
 
 function App() {
+  const [quote, setQuote] = useState('');
+
+  useEffect(() => {
+    novaFrase();
+  }, []);
+
+  const novaFrase = () => {
+    fetch('https://api.kanye.rest/')
+      .then(response => response.json())
+      .then(data => setQuote(data.quote))
+      .catch(error => console.error('Erro:', error));
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img className='kanyeImg' src={kanye} alt='imagem kanye'/>
+        <h2>{quote}</h2>
+        <button className='botaoGerar' onClick={novaFrase}>Gerar Nova Frase</button>
       </header>
     </div>
   );
